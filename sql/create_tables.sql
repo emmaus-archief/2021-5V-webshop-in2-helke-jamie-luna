09a76eb0a4806842e34dcdffe63ef5520d079f55
vangeest <49232516+vangeest@users.noreply.github.com>
2020-11-23 10:52:59 +0100

    werkt het?

---
+++ sql/create_tables.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS products; 
+CREATE TABLE products (
+  ID SERIAL PRIMARY KEY,
+  code VARCHAR(15),
+  name VARCHAR(255),
+  description TEXT,
+  color INTEGER,
+  soort INTEGER,
+  price NUMERIC(10, 2)
+);
+
+CREATE TABLE aanbevolen (
+  ID SERIAL PRIMARY KEY,
+  ID SERIAL PRIMARY KEY
+);
+
+ CREATE TABLE soort (
+   ID SERIAL PRIMARY KEY,
+   name VARCHAR(255)
+ );
+
+ CREATE TABLE color (
+   ID SERIAL PRIMARY KEY,
+   name VARCHAR(255),
+   description TEXT
+ );
+
+ nee
